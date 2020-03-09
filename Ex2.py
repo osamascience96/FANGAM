8bb59d8f187b1d6fa546db6eaaa3638f7e5c2c49
osamascience96 <osamahu96@gmail.com>
2020-03-09 07:26:26 +0500

    Exercise 2 of Microsoft Solution

---
+++ Ex2.py
@@ -0,0 +1,16 @@
+# Question asked by Microsoft
+# Find the solution in linear time without repeating characters.
+
+class Solution:
+    def lengthOfLongestSubString(self, s):
+        self.length = 0 # length variable contains the value of the longest substring
+        chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for i in chars:
+            # if the frequency of the char is more than 1,
+            x = s.count(i)
+            if (x > 1):
+                # add the number to the length 
+                self.length = self.length + x
+        return self.length
+
+print (Solution().lengthOfLongestSubString('abrkaabcdefghijjxxxoooo'))
